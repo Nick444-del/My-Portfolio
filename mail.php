76dd94be847222cc46509b4cc01153d720a8e7b7
Nick444-del <76736944+Nick444-del@users.noreply.github.com>
2021-12-31 20:35:09 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['msg'];
+
+$to = "nickgoruleax000e1000@gmail.com";
+
+$subject = "Mail From Website";
+$txt = "Name = ". $name . "\r\n  Email = ". $email . "\r\n Message = ". $message;
+$headers = "From: noreply@yoursite.com"."\r\n"."CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+header("Location:thankyou.html");
+?>
